1b492f886a394a36bf7e8c9d027eeadbb3c5d048
davdmoo <david.mulyawan97@gmail.com>
2024-09-09 21:35:43 +0700

    initial commit

---
+++ handlers/slinks.handler.ts
@@ -0,0 +1,45 @@
+import { Hono } from "hono";
+import { html } from "hono/html";
+import { nanoid } from "npm:nanoid";
+import { db } from "../db.ts";
+
+export const slinks = new Hono();
+
+slinks.post("/", async (c) => {
+  try {
+    const { url } = await c.req.parseBody();
+
+    // check if the url already exists in the DB
+    const existingSlinkQueryResult = await db.execute({
+      sql: "SELECT * FROM slinks WHERE url = (:url) LIMIT 1",
+      args: { url: url as string },
+    });
+
+    let slink: Record<string, unknown> | undefined;
+    const existingSlink = existingSlinkQueryResult.rows.at(0);
+    if (existingSlink === undefined) {
+      const id = nanoid(6);
+      const queryResult = await db.execute({
+        sql: "INSERT INTO slinks (id, url) VALUES (:id, :url) RETURNING *;",
+        args: { id, url: url as string },
+      });
+      slink = queryResult.rows.at(0);
+    } else {
+      slink = existingSlink;
+    }
+
+    if (slink === undefined) {
+      return c.html(html`Failed while trying to create a new slink`);
+    }
+
+    const redirectUrl = `http://127.0.0.1:8000/${slink.id}`;
+    return c.html(html`
+      <a href="${redirectUrl}" target="#blank">${redirectUrl}</a>
+    `);
+  } catch (err) {
+    console.error(err);
+    if (err instanceof Error) {
+      console.error(err.stack);
+    }
+  }
+});
---
+++ main.ts
@@ -0,0 +1,40 @@
+import { Hono } from "hono";
+import { serveStatic } from "hono/deno";
+import { Edge } from "npm:edge.js";
+import { db } from "./db.ts";
+import { slinks } from "./handlers/slinks.handler.ts";
+
+const app = new Hono();
+
+export const edge = Edge.create();
+edge.mount(new URL("./views", import.meta.url));
+
+app.use("/static/*", serveStatic({ root: "./" }));
+app.use("/favicon.ico", serveStatic({ path: "./static/icon.svg" }));
+
+app.get("/", async (c) => {
+  const indexPage = await edge.render("index");
+  return c.html(indexPage);
+});
+
+app.get("/:id", async (c) => {
+  const { id } = c.req.param();
+  const queryResult = await db.execute({
+    sql: "SELECT * FROM slinks WHERE id = (:id);",
+    args: { id },
+  });
+  const slink = queryResult.rows.at(0);
+  if (slink === undefined) {
+    const errorPage = await edge.render("error", {
+      statusCode: 404,
+      message: "Not found",
+    });
+    return c.html(errorPage);
+  }
+
+  return c.redirect(slink.url as string, 302);
+});
+
+app.route("/slinks", slinks);
+
+Deno.serve(app.fetch);
