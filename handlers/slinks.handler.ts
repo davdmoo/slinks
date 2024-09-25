a8a28ed90bc9b91fc39b68c9f54faf8dcb04b34b
davdmoo <david.mulyawan97@gmail.com>
2024-09-25 11:42:14 +0700

    refactor dark/light styles in css, remove shoelace style dependency

--- handlers/slinks.handler.ts
+++ handlers/slinks.handler.ts
@@ -41,7 +41,9 @@ slinks.post("/", async (c) => {
     return c.html(html`
       <div id="response" class="response">
         <p>${redirectUrl}</p>
-        <sl-icon-button name="copy" _="on click call navigator.clipboard.writeText('${redirectUrl}') then show #alert"></sl-icon-button>
+        <button style="border: none;" _="on click call navigator.clipboard.writeText('${redirectUrl}') then show #alert">
+          <svg width="20px" height="20px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M19.4 20H9.6C9.26863 20 9 19.7314 9 19.4V9.6C9 9.26863 9.26863 9 9.6 9H19.4C19.7314 9 20 9.26863 20 9.6V19.4C20 19.7314 19.7314 20 19.4 20Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M15 9V4.6C15 4.26863 14.7314 4 14.4 4H4.6C4.26863 4 4 4.26863 4 4.6V14.4C4 14.7314 4.26863 15 4.6 15H9" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+        </button>
       </div>
     `);
   } catch (err) {
